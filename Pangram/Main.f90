16af52504cac7a52bbd105c0145612d55d9fa0b9
Giorgi Maghlakelidze <acidlabz@gmail.com>
2013-03-01 19:37:42 +0400

    Intial commit, basic info

---
+++ Pangram/Main.f90
@@ -0,0 +1,21 @@
+! Program name: Pangram90
+! Author: Giorgi [DrAcid] Maghlakelidze
+! Purpose: check if provided string or set of 
+!          strings is a pangram. Created as an exercise
+! Usage: 
+!         a. prepare alphabet in a file abc.dat file
+!         b. prepare a list of string to be checked in pangrams.dat
+!         c. program will output a file with pangrams if such exist
+!
+! YEAR: 2013
+PROGRAM Pangram_checker
+
+IMPLICIT NONE
+
+
+
+
+WRITE(*,*) "Press ENTER to exit..."
+READ (*,*)
+
+END PROGRAM Pangram_checker
